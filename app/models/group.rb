711e96390dd3d8e781aba38b3d11e7d6c97b9f06
haruto <138562348+haru-shducks@users.noreply.github.com>
2023-11-25 08:45:58 +0000

    順番入れ替え

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
-  has_many :group_users, dependent: :destroy
   has_many :group_chats, dependent: :destroy
+  has_many :group_users, dependent: :destroy
 
   belongs_to :leader, class_name: "User"
 
